a49b8b7658d2f5ce90dd5e22d302a69b9206df8c
LeonidasVarveropoulos <leonidas@vrhome.net>
2019-11-07 22:34:06 -0600

    added rs launch file

--- scripts/robot_phy.sh
+++ scripts/robot_phy.sh
@@ -7,4 +7,4 @@ roscore -p 11311 &
 sleep 5
  
 # Start realsense
-roslaunch realsense2_camera rs_d400_and_t265.launch topic_odom_in:=/odom serial_no_camera1:=925122110604 camera1:=t265 serial_no_camera2:=927522071627 camera2:=d435 publish_odom_tf:=false
+roslaunch rs_launcher rs_d400_and_t265.launch topic_odom_in:=/odom serial_no_camera1:=925122110604 camera1:=t265 serial_no_camera2:=927522071627 camera2:=d435

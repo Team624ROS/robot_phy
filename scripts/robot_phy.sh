a77eeebefa1dbcfd12099cb5927385b172799b99
LeonidasVarveropoulos <leonidas@vrhome.net>
2019-11-08 19:05:33 -0600

    Added support for the rplidar

--- scripts/robot_phy.sh
+++ scripts/robot_phy.sh
@@ -7,4 +7,7 @@ roscore -p 11311 &
 sleep 5
  
 # Start realsense
-roslaunch rs_launcher rs_d400_and_t265.launch topic_odom_in:=/odom serial_no_camera1:=925122110604 camera1:=t265 serial_no_camera2:=927522071627 camera2:=d435 
+roslaunch rs_launcher rs_d400_and_t265.launch topic_odom_in:=/odom serial_no_camera1:=925122110604 camera1:=t265 serial_no_camera2:=927522071627 camera2:=d435 &
+
+chmod 666 /dev/rplidarUSB
+roslaunch rplidar rplidar.launch

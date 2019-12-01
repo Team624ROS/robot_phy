ccdb94df5e9b20f7499f72a8efd6ddbb45c17dda
LeonidasVarveropoulos <leonidas@vrhome.net>
2019-11-30 22:56:22 -0600

    updated comments

--- scripts/robot_phy.sh
+++ scripts/robot_phy.sh
@@ -5,8 +5,8 @@ source ~/robot_phy/devel/setup.bash
 # Start roscore and wait till its finished
 roscore -p 11311 &
 sleep 5
- 
-# Start realsense
+
+# Start realsense  # CHANGE CALB FILE PATH FOR ODOM
 roslaunch rs_launcher rs_d400_and_t265.launch topic_odom_in:=/odom serial_no_camera1:=925122110604 camera1:=t265 serial_no_camera2:=927522071627 camera2:=d435 &
 
 chmod 666 /dev/rplidarUSB
